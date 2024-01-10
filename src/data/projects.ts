3865c5a0499aca088b467a4b43acddc6fe770c53
Santiago Jimenez <santjimenez@utp.edu.co>
2024-01-10 13:27:38 -0500

    se actualizan links de los proyectos

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -24,6 +24,7 @@ const projects: Project[] = [
     description:
       "Una app para revivir aventuras de viaje a través de pines interactivos que relatan tus experiencias. Ideal para recordar y compartir tus historias.  ",
   },
+
   {
     title: "Profit Manager",
     techs: ["Svelte", "JavaScript", "Firebase", "Flowbite", "Firebase Auth"],
@@ -33,6 +34,34 @@ const projects: Project[] = [
     description:
       "Una app que gestiona tus finanzas con facilidad, puede controlar tus cuentas de ahorro, supervisa inversiones (CDTs, RentaAhorro), manejar tus préstamos a terceros.",
   },
+
+  {
+    title: "Pokedex",
+    techs: ["Vue.js", "JavaScript", "Html", "Css"],
+    git: "https://github.com/ssantss/Pokedex_vue",
+    link: "https://pokedex.paicoders.com/",
+    description:
+      "Pokedex, desarrollado con Vue, aprovecha la API de Pokeapi para acceder a todos los Pokémon y facilitar su búsqueda.",
+  },
+
+  {
+    title: "Rickandmorty Api test",
+    techs: ["JavaScript", "Html", "Css"],
+    git: "https://github.com/ssantss/rickandmortyApi",
+    link: "https://rickandmorty.paicoders.com/",
+    description:
+      "Este proyecto integra la API de Rick and Morty para crear una interfaz web interactiva utilizando HTML, CSS y JavaScript vanilla.",
+  },
+
+  {
+    title: "Geometric-calc",
+    techs: ["JavaScript", "Html", "Css"],
+    git: "https://github.com/ssantss/Geometric_calc",
+    link: "https://geometriccalc.paicoders.com/",
+    description:
+      "Este proyecto hace calculos geometricos, utilizando HTML, CSS y JavaScript vanilla.",
+  },
+
   {
     title: "IPhone stock checker",
     techs: ["Python"],
@@ -40,36 +69,13 @@ const projects: Project[] = [
     description:
       "Script para verificar la disponibilidad de productos Apple en el sitio web oficial de Apple en EE. UU., necesaria la inclusión de un código postal.",
   },
+
   {
     title: "Auto-Refreshify",
     techs: ["JavaScript", "Html", "Css"],
     git: "https://gitlab.com/paicoder/reload-page-chrome-extension",
     description:
       "Auto-Refreshify es una extensión de navegador que actualiza páginas automáticamente tras un periodo de inactividad, eliminando la necesidad de clics manuales.",
-  },
-  {
-    title: "Pokedex",
-    techs: ["Vue.js", "JavaScript", "Html", "Css"],
-    git: "https://github.com/ssantss/Pokedex_vue",
-    link: "https://pokedex-vue1.vercel.app/",
-    description:
-      "Pokedex, desarrollado con Vue, aprovecha la API de Pokeapi para acceder a todos los Pokémon y facilitar su búsqueda.",
-  },
-  {
-    title: "Rickandmorty Api test",
-    techs: ["JavaScript", "Html", "Css"],
-    git: "https://github.com/ssantss/rickandmortyApi",
-    link: "https://rickandmorty-api1.vercel.app/",
-    description:
-      "Este proyecto integra la API de Rick and Morty para crear una interfaz web interactiva utilizando HTML, CSS y JavaScript vanilla.",
-  },
-  {
-    title: "Geometric-calc",
-    techs: ["JavaScript", "Html", "Css"],
-    git: "https://github.com/ssantss/Geometric_calc",
-    link: "https://geometric-calc1.vercel.app/",
-    description:
-      "Este proyecto hace calculos geometricos, utilizando HTML, CSS y JavaScript vanilla.",
   },
 ];
 
